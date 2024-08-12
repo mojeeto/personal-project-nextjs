170f35904abf77d3794255dba8735f7504211087
Mojeeto <mojtaba01nouri@gmail.com>
2024-08-12 19:28:56 +0330

    [feat] finance page
    Init finance page in dashboard route group.

---
+++ src/app/(dashboard)/finance/page.tsx
@@ -0,0 +1,10 @@
+export default function FinancePage() {
+  return (
+    <main>
+      <div className="flex flex-row justify-between">
+        <h2>$300,000 Available</h2>
+        <button>New Record</button>
+      </div>
+    </main>
+  );
+}
