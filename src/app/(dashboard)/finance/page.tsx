1807a2b0ce049366efe136f887a962915cfa1cc4
Mojeeto <mojtaba01nouri@gmail.com>
2024-08-12 19:38:29 +0330

    [feat] no style table for finance page

--- src/app/(dashboard)/finance/page.tsx
+++ src/app/(dashboard)/finance/page.tsx
@@ -1,3 +1,13 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function FinancePage() {
   return (
     <main>
@@ -5,6 +15,27 @@ export default function FinancePage() {
         <h2>$300,000 Available</h2>
         <button>New Record</button>
       </div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[24px]">#</TableHead>
+            <TableHead>Subject</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Type</TableHead>
+            <TableHead>Description</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">1</TableCell>
+            <TableCell>Buy Drink</TableCell>
+            <TableCell>$300</TableCell>
+            <TableCell>SELL</TableCell>
+            <TableCell>Buy two coffee for my self and my brother.</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </main>
   );
 }
