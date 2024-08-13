651a865ce34ea2593de67cd58904139b3e8ee9c3
Mojeeto <mojtaba01nouri@gmail.com>
2024-08-13 10:03:22 +0330

    [feat] finance table
    table that show records of finance.

--- src/app/(dashboard)/finance/page.tsx
+++ src/app/(dashboard)/finance/page.tsx
@@ -1,12 +1,4 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import FinanceTable from "@/components/finance-table";
 
 export default function FinancePage() {
   return (
@@ -15,27 +7,7 @@ export default function FinancePage() {
         <h2>$300,000 Available</h2>
         <button>New Record</button>
       </div>
-      <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[24px]">#</TableHead>
-            <TableHead>Subject</TableHead>
-            <TableHead>Amount</TableHead>
-            <TableHead>Type</TableHead>
-            <TableHead>Description</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">1</TableCell>
-            <TableCell>Buy Drink</TableCell>
-            <TableCell>$300</TableCell>
-            <TableCell>SELL</TableCell>
-            <TableCell>Buy two coffee for my self and my brother.</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+      <FinanceTable />
     </main>
   );
 }
---
+++ src/components/finance-table.tsx
@@ -0,0 +1,37 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const FinanceTable: React.FC = () => {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[24px]">#</TableHead>
+          <TableHead>Subject</TableHead>
+          <TableHead>Amount</TableHead>
+          <TableHead>Type</TableHead>
+          <TableHead>Description</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">1</TableCell>
+          <TableCell>Buy Drink</TableCell>
+          <TableCell>$300</TableCell>
+          <TableCell>SELL</TableCell>
+          <TableCell>Buy two coffee for my self and my brother.</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+};
+
+export default FinanceTable;
